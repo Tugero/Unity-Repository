262f37e74b3ec31571999a3de2c85708816af4de
Trash Panda <dcortell@verizon.net>
2019-02-15 07:23:26 -0500

    UFO tutorial Added
    The UFO Tutorial without challenges was completed with extra steps and added

---
+++ UFO Tutorial Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private int count;
+
+    public Text countText;
+    public Text WinText;
+    public float speed;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        setCountText();
+        WinText.text = " ";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal,moveVertical);
+        rb2d.AddForce(movement*speed);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+    }
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            WinText.text = "!!You Win!!";
+        }
+    }
+}
