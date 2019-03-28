2621642f91657d2d21140aedd9a5f523ccaa8dbb
Trash Panda <dcortell@verizon.net>
2019-03-28 18:43:57 -0400

    Things Added
    Space Shooter Tutorial and other things

---
+++ Production Final Codingplatform/Assets/Scripts/DamageCalculations.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class DamageCalculations : MonoBehaviour
+{
+    public float MHP = 100.0f;
+    private Vector3 spawnPoint;
+    private float CHP;
+    public GameObject mob;
+
+    void Start()
+    {
+        CHP = MHP;
+    }
+
+    public void TakeDamage(float DMG)
+    {
+        CHP -= DMG;
+        Debug.Log(""+CHP);
+        if(CHP <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        Debug.Log(gameObject);
+        Debug.Log("Was Killed!");
+        Destroy(gameObject);
+    }
+}
+
---
+++ Production Final Codingplatform/Assets/Scripts/Damager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy")){
+            other.gameObject.SetActive(false);
+        }
+    }
+}
